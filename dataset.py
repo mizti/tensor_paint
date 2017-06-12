fbf2de70c659b4d40605322d9082ab99ca883ade
Keita Shimizu <miz@shimizukeita-no-MacBook.local>
2017-06-13 08:15:17 +0900

    dataset worked

--- dataset.py
+++ dataset.py
@@ -4,10 +4,44 @@ import numpy as np
 class PaintData():
   def __init__(self):
     pass
-
+  # this may be a help: https://gist.github.com/leVirve/80428277f2d14515870fa18899fed17a
   def get_dataset(self):
+    #filename_queue = tf.train.string_input_producer(tf.train.match_filenames_once('*'), num_epochs=1, shuffle=True)
+    #filenames = tf.train.match_filenames_once('./data/*')
+    filenames = tf.train.match_filenames_once('data/*.jpg')
+
+    #with tf.Session() as sess:
+    #  #tf.global_variables_initializer().run()
+    #  tf.local_variables_initializer().run() # why not global?
+    #  print(sess.run(filenames))
+    #  print('======') 
+    #exit()
+    filename_queue = tf.train.string_input_producer(filenames)
+
+    reader = tf.WholeFileReader()
+    #_, value = reader.read(filename_queue)
+    _, value = reader.read(filename_queue)
+    
+    image = tf.image.decode_jpeg(value, channels=3)
+    image = tf.image.resize_images(image, [100, 100])
+    
+    with tf.Session() as sess:
+    
+      #tf.global_variables_initializer().run()
+      tf.local_variables_initializer().run()
+      coord = tf.train.Coordinator()
+      threads = tf.train.start_queue_runners(coord=coord)
+    
+      image_tensor = sess.run([image])
+      print(image_tensor)
+    
+      coord.request_stop()
+      coord.join(threads)
+      return image_tensor, "hoge"
+
+  def get_dataset2(self):
     # creating Tensor data
-    #filelist = ["data/1.jpg", "data/2.jpg", "data/3.jpg"]
+    filelist = ["data/1.jpg", "data/2.jpg", "data/3.jpg"]
     #filequeue = tf.train.string_input_producer(filelist)
     
     #data = tf.random_uniform([i], -1, i)
